da65d735285aef162dd6744e9b0137fe377ea849
Jazmin <ACER@LAPTOP-FRMPB979>
2025-05-25 04:16:44 -0600

    cambios

--- ejercicio2.java
+++ ejercicio2.java
@@ -1,3 +1,34 @@
-public class ejercicio2 {
+abstract class Personal {
+    public abstract void registrar();
+    public abstract double calcularSueldo();
+}
 
+class Empleado extends Personal {
+    private String nombre;
+    private double horasTrabajadas;
+    private double pagoPorHora;
+
+    public Empleado(String nombre, double horasTrabajadas, double pagoPorHora) {
+        this.nombre = nombre;
+        this.horasTrabajadas = horasTrabajadas;
+        this.pagoPorHora = pagoPorHora;
+    }
+
+    @Override
+    public void registrar() {
+        System.out.println("Empleado registrado: " + nombre);
+    }
+
+    @Override
+    public double calcularSueldo() {
+        return horasTrabajadas * pagoPorHora;
+    }
+}
+
+public class ejercicio2 {
+    public static void main(String[] args) {
+        Empleado emp = new Empleado("Juan", 40, 15.5);
+        emp.registrar();
+        System.out.println("Sueldo: $" + emp.calcularSueldo());
+    }
 }
--- jercicio1.java
+++ jercicio1.java
@@ -1,3 +1,38 @@
-public class jercicio1 {
+class Vehiculo {
+    public void mover() {
+        System.out.println("El vehículo se está moviendo.");
+    }
+}
 
+class Automovil extends Vehiculo {
+    @Override
+    public void mover() {
+        System.out.println("El automóvil circula por carretera.");
+    }
+}
+
+class Barco extends Vehiculo {
+    @Override
+    public void mover() {
+        System.out.println("El barco navega por el agua.");
+    }
+}
+
+class Avion extends Vehiculo {
+    @Override
+    public void mover() {
+        System.out.println("El avión vuela por el aire.");
+    }
+}
+
+public class jercicio1 {
+    public static void main(String[] args) {
+        Vehiculo v1 = new Automovil();
+        Vehiculo v2 = new Barco();
+        Vehiculo v3 = new Avion();
+
+        v1.mover();
+        v2.mover();
+        v3.mover();
+    }
 }
